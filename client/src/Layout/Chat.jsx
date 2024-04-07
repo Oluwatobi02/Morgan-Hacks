fdc5110da8eb4ea7098b35740a3f8d5a3f5a7708
Oluwatobi02 <olola73@morgan.edu>
2024-04-07 01:44:22 -0400

    code reset

--- client/src/Layout/Chat.jsx
+++ client/src/Layout/Chat.jsx
@@ -3,40 +3,26 @@ import { motion } from "framer-motion";
 import './Chat.css';
 
 const Chat = () => {
-    const [showQuestion, setShowQuestion] = useState(true)
-    const [userInput, setUserInput] = useState("")
-    const [displayInput, setDisplayInput ] = useState("")
-    const handleClick = () => {
-        setShowQuestion(false)
-    }
-    const handleKeyDown = (event) => {
-        if (event.key === 'Enter') {
-            setDisplayInput(userInput)
-            setShowQuestion(false)
-            
-        }
-    }
-    const handleChange = (event) =>{
-        console.log(event.target)
-        console.log("Value: ", event.target.value)
-       setUserInput( event.target.value)
-    }
+
+
+
 
     return (
         <div className="chat">
             <div className="bot-message">
-            {showQuestion? 
-                <p>How do you feel about your current major do you think it matches your strenght and weaknesses?</p>: <p> { displayInput }</p>
-            }
+            <span>How do you feel about your current major do you think it matches your strenght and weaknesses?</span>
             </div> 
 
       
             
             <div className="messagebox">
+    
+                <input className="textbox" 
+                        type="text"  s
+                        style={ { color:"black" }  } 
+                        />
 
-                <input className="textbox" type="text"  style={ { color:"black" }  } onChange={handleChange } value={ userInput} onKeyDown={ handleKeyDown }/>
-
-                <button  className="chatbtn" onClick={handleClick} >Send</button>
+                <button className="chatbtn" >Send</button>
             </div>
         </div>
     );
